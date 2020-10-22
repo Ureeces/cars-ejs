9d9efea98ace6bda63777eb14ced5ccc05ebf74e
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-10-22 16:06:32 -0400

    Adds unfinished work for adding cars.

--- routes/Cars/carRoutes.js
+++ routes/Cars/carRoutes.js
@@ -5,8 +5,22 @@ const {
     createCarr
 } = require('./controllers/carController');
 
+// GET Routes
+// *******************************************************************
+router.get('/', (req, res) => {
+    return res.redirect('/cars/get-cars');
+});
+
 router.get('/get-cars', getAllCars);
 
+router.get('add-car', (req, res) => {
+    return res.render('main/add-car');
+});
+
+// *******************************************************************
+// POST Routes
 router.post('/add-car', createCarr);
 
 module.exports = router;
+
+// *******************************************************************
--- routes/Cars/controllers/carController.js
+++ routes/Cars/controllers/carController.js
@@ -10,6 +10,27 @@ module.exports = {
     },
 
     createCarr: (req, res) => {
+        Car.findOne({ name: req.body.name })
+        .then((existingCar) => {
+            if(existingCar) {
+                res.send('Car already exists within list.');
+            } else {
+                if(!req.body.name || req.body.type || req.body.year) {
+                    return res.send('All Inputs Are Required.');
+                }
 
+                let newCar = new Car({
+                    name: req.body.name,
+                    type: req.body.type,
+                    year: req.body.year,
+                });
+
+                newCar.save()
+                .then((createdCar) => {
+                    return res.redirect('/cars/get-cars');
+                })
+                .catch((err) => res.status(500).json({ message: 'Word Was Not Created:', err })) 
+            }
+        })
     }
 }
