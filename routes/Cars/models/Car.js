67cf8b4793cd99bdcce04b567e0b0378261f36e8
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-10-22 14:23:23 -0400

    Adds Car Schema.

---
+++ routes/Cars/models/Car.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const CarSchema = new mongoose.Schema({
+    name: { type: String, lowercase: true, required: true, unique: true },
+    type: { type: String, required: true },
+    year: { type: String, required: true }
+});
+
+module.exports = mongoose.model('car', CarSchema);
